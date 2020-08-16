6a3fe1d4c35528fb2f4ca3d822b597cb2f668fca
bulut67 <mahirbulut67@gmail.com>
2020-08-16 23:07:47 +0300

    sondenemebranch

--- src/git_project_sonders/SonDersTekrar.java
+++ src/git_project_sonders/SonDersTekrar.java
@@ -5,6 +5,8 @@ public class SonDersTekrar {
 	public static void main(String[] args) {
 				
 			System.out.println("21.55==>master branch");
+			System.out.println("23.23==>**feature branch**");
+			System.out.println("23.23==>**feature branch'i master'a ekledik");
 	}
 
 }
